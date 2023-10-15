dda8c2c61126e78d646a5bbba76539640e68e45b
Feng Zhu <fzhu2e@outlook.com>
2023-10-14 19:48:47 -0600

    Update bin

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='c4p',  # required
-    version='2023.10.12',
+    version='2023.10.14',
     description='c4p: CESM for Paleo',
     long_description=long_description,
     long_description_content_type='text/x-rst',
