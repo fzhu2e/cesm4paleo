ca52e234ae773e41ea55d60fef30a88abc35df98
Feng Zhu <fzhu2e@outlook.com>
2024-01-20 14:07:29 -0700

    Fix version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='c4p',  # required
-    version='2023.1.20',
+    version='2024.1.20',
     description='c4p: CESM for Paleo',
     long_description=long_description,
     long_description_content_type='text/x-rst',
